3fcbf7643478dd5cb13d31d1d6593541a66a73a1
carlos-ufv <65286924+carlos-ufv@users.noreply.github.com>
2020-11-18 01:11:09 +0100

    Create LeerCSV.java

---
+++ src/main/java/org.dis/LeerCSV.java
@@ -0,0 +1,15 @@
+// lectura del fichero csv mediante el objeto plano y su carga en una lista de objeto de este tipo
+package ...;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+// Otros imports ...
+
+public class LeerCSV {
+
+    public List<golfPlayer> parseaCSV(String path) {
+    ....
+    return players;
+    }
+}
