24e3f3cf9b59584eecf54bd51ad511859e215805
Silvia <silviaromazp+1@gmail.com>
2020-11-19 21:00:24 +0100

    Renombrando la clase a golfPlayer y getters y setters de los atributos

--- src/main/java/org.dis/EscribeJSON.java
+++ src/main/java/org.dis/EscribeJSON.java
@@ -1,11 +1,10 @@
 //Fichero para mostrar por consola el JSON generado a partir del CSV y almacenarlo en el fichero best-golf-players.json
-package ...
-imports ...
+package org.dis;
 
 public class EscribeJSON {
 
     public static void main(String[] args) {
-    ....
+
     }
     
  }
--- src/main/java/org.dis/golfPlayer.java
+++ src/main/java/org.dis/golfPlayer.java
@@ -1,7 +1,56 @@
-public class POJO {
-        ... otros atributos ...
-    @CSVBindByName
-    private String Columna1;
-        ... otros atributos ...
-            ....getters & setters...
+package org.dis;
+
+public class golfPlayer {
+    public golfPlayer() {
+
+    }
+    //@CSVBindByName
+    //Posicion, Nombre, Apellido, Pais, Edad
+
+    private String Posicion;
+    private String Nombre;
+
+    public String getPosicion() {
+        return Posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        Posicion = posicion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String pais) {
+        Pais = pais;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String edad) {
+        Edad = edad;
+    }
+
+    private String Apellido;
+    private String Pais;
+    private String Edad;
 }
