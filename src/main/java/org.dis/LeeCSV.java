7b4394ecaf1e2612d31204c5bbe07eec61d047a4
Silvia <silviaromazp+1@gmail.com>
2020-11-19 21:41:10 +0100

    funciona

--- src/main/java/org.dis/LeeCSV.java
+++ src/main/java/org.dis/LeeCSV.java
@@ -10,23 +10,22 @@ import java.io.BufferedReader;
 import java.util.List;
 
 
-public class LeerCSV {
+public class LeeCSV {
 
     public List<golfPlayer> parseaCSV(String path) {
 
-        CsvToBean csvToBean = null;
+        BufferedReader fileReader = null;
         try {
-            List<golfPlayer> players = CsvToBean.parse();
-            BufferedReader fileReader = new BufferedReader(new FileReader("best-golf-players.csv"));
-            csvToBean = new CsvToBeanBuilder(fileReader) //Lista de beans para cargar el csv
-                    .withType(golfPlayer.class)
-                    .withIgnoreLeadingWhiteSpace(true) // Para ignorar blancos
-                    .build();
+            fileReader = new BufferedReader(new FileReader("best-golf-players.csv"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
+        CsvToBean csvToBean = new CsvToBeanBuilder(fileReader) //Lista de beans para cargar el csv
+                    .withType(golfPlayer.class)
+                    .withIgnoreLeadingWhiteSpace(true) // Para ignorar blancos
+                    .build();
         List<golfPlayer> players = csvToBean.parse();
+
         return players;
     }
 }
