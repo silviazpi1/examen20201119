456ee66181e628d31c2bb948aa3a6dfd58eed735
Silvia <silviaromazp+1@gmail.com>
2020-11-19 21:09:20 +0100

    CsvBindByName funciona

--- src/main/java/org.dis/LeeCSV.java
+++ src/main/java/org.dis/LeeCSV.java
@@ -1,10 +1,6 @@
 // lectura del fichero csv mediante el objeto plano y su carga en una lista de objetos de este tipo
 package org.dis;
 
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
-
-// Otros imports ...
 
 public class LeerCSV {
 
--- src/main/java/org.dis/golfPlayer.java
+++ src/main/java/org.dis/golfPlayer.java
@@ -1,56 +1,25 @@
 package org.dis;
 
+import com.opencsv.bean.CsvBindByName;
+
 public class golfPlayer {
     public golfPlayer() {
 
     }
-    //@CSVBindByName
-    //Posicion, Nombre, Apellido, Pais, Edad
 
-    private String Posicion;
+    @CsvBindByName(column = "Nombre", required = true)
     private String Nombre;
 
-    public String getPosicion() {
-        return Posicion;
-    }
-
-    public void setPosicion(String posicion) {
-        Posicion = posicion;
-    }
-
-    public String getNombre() {
-        return Nombre;
-    }
-
-    public void setNombre(String nombre) {
-        Nombre = nombre;
-    }
-
-    public String getApellido() {
-        return Apellido;
-    }
-
-    public void setApellido(String apellido) {
-        Apellido = apellido;
-    }
-
-    public String getPais() {
-        return Pais;
-    }
-
-    public void setPais(String pais) {
-        Pais = pais;
-    }
-
-    public String getEdad() {
-        return Edad;
-    }
-
-    public void setEdad(String edad) {
-        Edad = edad;
-    }
-
+    @CsvBindByName(column = "Apellido", required = true)
     private String Apellido;
+
+    @CsvBindByName(column = "Posicion", required = true)
+    private String Posicion;
+
+    @CsvBindByName(column = "Pais", required = true)
     private String Pais;
+
+    @CsvBindByName(column = "Edad", required = true)
     private String Edad;
+
 }
