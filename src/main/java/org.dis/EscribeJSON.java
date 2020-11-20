56c5371e06435d6e16237f39cd81ce3f22baf0e4
Silvia <silviaromazp+1@gmail.com>
2020-11-20 10:38:33 +0100

    Parsea el csv

--- src/main/java/org.dis/EscribeJSON.java
+++ src/main/java/org.dis/EscribeJSON.java
@@ -1,16 +1,23 @@
 //Fichero para mostrar por consola el JSON generado a partir del CSV y almacenarlo en el fichero best-golf-players.json
 package org.dis;
 
+import com.google.gson.JsonObject;
+
+import java.util.List;
+
 public class EscribeJSON {
 
     public static void main(String[] args) {
-        int num_jugadores = 5;
-        //golfPlayer[] ListaJugadores = new golfPlayer()[num_jugadores];
-        //LeerCSV("best-golf-players.csv");
 
-        for(int i = 0; i< num_jugadores; i++){
+        LeeCSV FicheroCsv = new LeeCSV();
+        List<golfPlayer> jugadores = FicheroCsv.parseaCSV("./best-golf-players.csv");
+        int numero_jugadores = jugadores.size();
 
+        JsonObject FicheroJson = new JsonObject();
+        FicheroJson.getAsJsonArray();
+
+        for(int i = 0; i < numero_jugadores; i++){
+            //FicheroJson
         }
     }
-    
  }
